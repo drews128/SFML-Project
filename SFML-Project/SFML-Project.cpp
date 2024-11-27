150f684124155cc54506b230645d4e8348478ab7
drews128 <drewschneider@charter.net>
2024-11-27 15:53:36 -0600

    SFML inlcudes

--- SFML-Project/SFML-Project.cpp
+++ SFML-Project/SFML-Project.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+//SFML files
 #include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/System.hpp"
 
 int main()
 {
-    //Test code
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    //Test
-
-    //Testing testing 123
+    
 
     return 1;
 }
